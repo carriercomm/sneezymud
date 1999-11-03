eb2928196f009e7c911142316ed2064ae87ef83d
lapsos <lapsos@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
1999-11-03 20:26:59 +0000

    Minor fix.

--- code/task_stavecharge.cc
+++ code/task_stavecharge.cc
@@ -2,17 +2,6 @@
 //
 // SneezyMUD - All rights reserved, SneezyMUD Coding Team
 //
-// $Log: task_stavecharge.cc,v $
-// Revision 5.1.1.1  1999/10/16 04:32:20  batopr
-// new branch
-//
-// Revision 5.1  1999/10/16 04:31:17  batopr
-// new branch
-//
-// Revision 1.1  1999/09/12 17:24:04  sneezy
-// Initial revision
-//
-//
 //////////////////////////////////////////////////////////////////////////
 
 
@@ -416,7 +405,7 @@ int TStaff::taskChargeMe(TBeing *ch, spellNumT tSpell, int & tCharge)
 
   int tManaCost = min(ch->getMana(), tManaReq);
 
-  ch->addToMana(-tManaCost);
+  ch->reconcileMana(TYPE_UNDEFINED, 0, tManaCost);
 
   tManaCost = tManaReq - tManaCost;
 

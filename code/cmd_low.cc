7e933431ae2409ad7479bd8e3a7de7b37ff4b01e
lapsos <lapsos@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
1999-12-19 21:54:12 +0000

    typo fix.

--- code/cmd_low.cc
+++ code/cmd_low.cc
@@ -1526,7 +1526,7 @@ void TBeing::lowWeaps(const char *arg)
         (blunt  && tbw->isBluntWeapon()) ||
         (pierce && tbw->isPierceWeapon()) ||
         (slash  && tbw->isSlashWeapon())) {
-      vlogf(-1, "Pushing Back Object: %d:%d", objnum, obj->objVnum());
+      vlogf(LOG_SILENT, "Pushing Back Object: %d:%d", objnum, obj->objVnum());
       objList.push_back(objnum);
     }
 

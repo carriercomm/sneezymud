97d1dafd99a6b32a68601461e7887f618e16651c
batopr <batopr@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2000-01-03 18:53:15 +0000

    removed $LOG

--- code/handler.h
+++ code/handler.h
@@ -2,24 +2,9 @@
 //
 // SneezyMUD - All rights reserved, SneezyMUD Coding Team
 //
-// $Log: handler.h,v $
-// Revision 5.1.1.1  1999/10/16 04:32:20  batopr
-// new branch
-//
-// Revision 5.1  1999/10/16 04:31:17  batopr
-// new branch
-//
-// Revision 1.1  1999/09/12 17:24:04  sneezy
-// Initial revision
-//
+// Usage: Various routines for moving about objects/players               
 //
 //////////////////////////////////////////////////////////////////////////
-
-
-/* ************************************************************************
-*  file: handler.h , Handler module.                      Part of DIKUMUD *
-*  Usage: Various routines for moving about objects/players               *
-************************************************************************* */
 
 #ifndef __HANDLER_H
 #define __HANDLER_H

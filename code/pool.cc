7c6027bdc92e4734288a5ec3cb1ef5e1983d3028
batopr <batopr@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2000-01-07 15:37:44 +0000

    removed $log

--- code/pool.cc
+++ code/pool.cc
@@ -2,23 +2,10 @@
 //
 // SneezyMUD - All rights reserved, SneezyMUD Coding Team
 //
-// $Log: pool.cc,v $
-// Revision 5.1.1.1  1999/10/16 04:32:20  batopr
-// new branch
-//
-// Revision 5.1  1999/10/16 04:31:17  batopr
-// new branch
-//
-// Revision 1.1  1999/09/12 17:24:04  sneezy
-// Initial revision
-//
-//
 //////////////////////////////////////////////////////////////////////////
 
 
 #include "stdsneezy.h"
-
-// this is Peel's code
 
 void TPool::setDrinkUnits(int n)
 {

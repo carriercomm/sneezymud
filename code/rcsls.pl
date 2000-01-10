8b3a3bd7a08608700e2376089f637e888cefbad3
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2000-01-10 19:29:25 +0000

    added checks for branch and head added -l flag to rlog, speeds things up

--- code/rcsls.pl
+++ code/rcsls.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl  -- Peel
 
-foreach(`rlog *.cc *.h makefile README`){
+foreach(`rlog -l *.cc *.h makefile README`){
   $fileName=$1 if /Working file: (.*)/;
   print "$fileName *** $_" if /locked by/;
+  print "$fileName *** $_" if /^branch: / && !/5.1.1$/;
+  print "$fileName *** $_" if /^head: / && !/5.1$/;
 }

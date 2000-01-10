78adaa51ee5cde39fd9e98a2379c06b073c39ee8
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2000-01-10 22:32:27 +0000

    changed formatting so its pretty

--- code/rcsls.pl
+++ code/rcsls.pl
@@ -2,7 +2,7 @@
 
 foreach(`rlog -l *.cc *.h makefile README`){
   $fileName=$1 if /Working file: (.*)/;
-  print "$fileName *** $_" if /locked by/ || 
-                             (/^branch: / && !/5.1.1$/) || 
-                             (/^head: / && !/5.1$/);
+  printf("%-16s *** $_", $fileName) if /locked by/ || 
+                                      (/^branch: / && !/5.1.1$/) || 
+			               (/^head: / && !/5.1$/);
 }

e2e272f7bff67572db1aa006460d44716f372674
peel <peel@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2000-01-11 02:19:43 +0000

    *** empty log message ***

--- code/rcsls.pl
+++ code/rcsls.pl
@@ -3,6 +3,5 @@
 foreach(`rlog -l *.cc *.h makefile README`){
   $fileName=$1 if /Working file: (.*)/;
   printf("%-16s *** $_", $fileName) if /locked by/ || 
-                                      (/^branch: / && !/5.1.1$/) || 
-			               (/^head: / && !/5.1$/);
+                        (/^branch: / && !/5.1.1$/) || (/^head: / && !/5.1$/);
 }

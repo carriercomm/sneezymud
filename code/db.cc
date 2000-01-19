1ce3a37e0988c6f7cb3eaa4deb195d310ffc2f7d
batopr <batopr@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2000-01-19 20:38:55 +0000

    modularized the weather.sunlight initialization stuff

--- code/db.cc
+++ code/db.cc
@@ -313,72 +313,14 @@ void reset_time(void)
   time_info.year += YEAR_ADJUST;
   moontype = time_info.day;
 
-  switch (time_info.hours) {
-    case 0:
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-      weather_info.sunlight = SUN_DARK;
-      break;
-    case 10:
-    case 11:
-    case 12:
-    case 13:
-      weather_info.sunlight = SUN_RISE;
-      break;
-    case 14:
-    case 15:
-    case 16:
-    case 17:
-    case 18:
-    case 19:
-    case 20:
-    case 21:
-    case 22:
-    case 23:
-    case 24:
-    case 25:
-    case 26:
-    case 27:
-    case 28:
-    case 29:
-    case 30:
-    case 31:
-    case 32:
-    case 33:
-    case 34:
-    case 35:
-      weather_info.sunlight = SUN_LIGHT;
-      break;
-    case 36:
-    case 37:
-    case 38:
-    case 39:
-      weather_info.sunlight = SUN_SET;
-      break;
-    case 40:
-    case 41:
-    case 42:
-    case 43:
-    case 44:
-    case 45:
-    case 46:
-    case 47:
-      weather_info.sunlight = SUN_DARK;
-      break;
+  calcNewSunRise();
+  calcNewSunSet();
 
-    default:
-      weather_info.sunlight = SUN_DARK;
-      break;
-  }
+  extern void fixSunlight();
+  fixSunlight();
 
-  vlogf(LOG_MISC, "   Current Gametime: %dH %dD %dM %dY.", time_info.hours, time_info.day, time_info.month, time_info.year);
+  vlogf(LOG_MISC, "   Current Gametime: %dm, %dH %dD %dM %dY.", 
+        time_info.minutes, time_info.hours, time_info.day, time_info.month, time_info.year);
 
   weather_info.pressure = 960;
   if ((time_info.month >= 7) && (time_info.month <= 12))
@@ -403,8 +345,6 @@ void reset_time(void)
   } else {
     weather_info.sky = SKY_CLOUDLESS;
   }
-  calcNewSunRise();
-  calcNewSunSet();
 }
 
 

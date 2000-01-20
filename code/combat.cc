52e588ebec6d1e7412bcede11bbdba1c5b4f83c4
batopr <batopr@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2000-01-20 14:37:32 +0000

    Added new SET_TRAP stuff

--- code/combat.cc
+++ code/combat.cc
@@ -3923,7 +3923,10 @@ immuneTypeT getTypeImmunity(spellNumT type)
     case SKILL_POISON_WEAPON:
     case SKILL_DISGUISE:
     case SKILL_DODGE_THIEF:
-    case SKILL_SET_TRAP:
+    case SKILL_SET_TRAP_CONT:
+    case SKILL_SET_TRAP_DOOR:
+    case SKILL_SET_TRAP_MINE:
+    case SKILL_SET_TRAP_GREN:
     case SKILL_DUAL_WIELD_THIEF:
     case SKILL_COUNTER_STEAL:
     case SKILL_BREW:

670710074131dc48801201688742a7125981d582
batopr <batopr@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2000-01-27 18:35:27 +0000

    Added in_range

--- code/extern.h
+++ code/extern.h
@@ -435,6 +435,7 @@ extern int age_mod_for_stat(int age_num, statTypeT whichStat);
 extern string describeDuration(const TBeing *, int);
 extern bool is_ok(TMonster *, TBeing *, int);
 extern int compareDetermineMessage(const int tDrift, const int tValue);
+extern bool in_range(int, int, int);
 }
 
 // these needs C++ linkage to avoid conflict with functions in stdlib

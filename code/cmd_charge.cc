075384ee1a6a7e815338f294b2724dc004e84dda
lapsos <lapsos@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2000-03-21 14:38:30 +0000

    Added a newline to the 'try to get in position' message.

--- code/cmd_charge.cc
+++ code/cmd_charge.cc
@@ -82,7 +82,7 @@ static int charge(TBeing *ch, TBeing *vict)
   if ((vict->getPosition() > POSITION_SITTING) &&
       ::number(0,2) &&
       ch->isTanking()) {
-    ch->sendTo(COLOR_MOBS, "You try to get in position to take a charge at %s.",
+    ch->sendTo(COLOR_MOBS, "You try to get in position to take a charge at %s.\n\r",
 vict->getName());
     ch->sendTo(COLOR_MOBS, "However, %s stays close to you.\n\rYou can't get the space needed to charge!\n\r", vict->getName());
     ch->cantHit += ch->loseRound(1);

d04fbad54a771f0b1e02b440310d33db6ee26dbe
lapsos <lapsos@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2000-04-02 00:53:19 +0000

    Added check to prevent summoning aggy/aggressive mobs to GH.

--- code/disc_hand_of_god.cc
+++ code/disc_hand_of_god.cc
@@ -491,13 +491,21 @@ int summon(TBeing * caster, TBeing * victim, int level, byte bKnown)
    *
    * The higher the result, the harder it is to summon
    */
-  if (tmon && !caster->isImmortal() && mob_index[tmon->getMobIndex()].max_exist < 10) {
-    int tDiff = ((50 - min((short) 10, mob_index[tmon->getMobIndex()].max_exist)) + (caster->GetMaxLevel() - victim->GetMaxLevel()));
+  if (tmon && (!tmon->master || !tmon->isAffected(AFF_CHARM) ||
+               !tmon->master->isPc()) && !caster->isImmortal()) {
+    if (mob_index[tmon->getMobIndex()].max_exist < 10) {
+      int tDiff = ((50 - min((short) 10, mob_index[tmon->getMobIndex()].max_exist)) + (caster->GetMaxLevel() - victim->GetMaxLevel()));
 
-    if ((tDiff > 0) && ::number(0, tDiff) &&
-        (!tmon->master || !tmon->isAffected(AFF_CHARM) ||
-         !tmon->master->isPc())) {
-      caster->sendTo("Your prayer meets opposition and fails!\n\r");
+      if ((tDiff > 0) && ::number(0, tDiff)) {
+        caster->sendTo("Your prayer meets opposition and fails!\n\r");
+        act("Nothing seems to happen.", FALSE, caster, NULL, NULL, TO_ROOM);
+        return SPELL_FAIL;
+      }
+    }
+
+    if (caster->inGrimhaven() &&
+        (tmon->anger() >= 20 || IS_SET(tmon->specials.act, ACT_AGGRESSIVE))) {
+      caster->sendTo("Your deity prohibits this mob being summoned here!\n\r");
       act("Nothing seems to happen.", FALSE, caster, NULL, NULL, TO_ROOM);
       return SPELL_FAIL;
     }

99a2349cfa47a767819871b15442c8de5ddcc3e4
jesus <jesus@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2001-03-14 00:44:37 +0000

    *** empty log message ***

--- code/connect.cc
+++ code/connect.cc
@@ -42,8 +42,8 @@ const int FORCE_LOW_INVSTE = 1;
 
 static const char * const WIZLOCK_PASSWORD           = "motelvi";
 const char * const MUD_NAME      = "Grimhaven";
-const char * const MUD_NAME_VERS = "Grimhaven v5.1";
-static const char * const WELC_MESSG = "\n\rWelcome to Grimhaven 5.1! May your journeys be interesting!\n\r\n\r";
+const char * const MUD_NAME_VERS = "Grimhaven v5.2";
+static const char * const WELC_MESSG = "\n\rWelcome to Grimhaven 5.2! May your journeys be interesting!\n\r\n\r";
 
 static const char * const TER_HUMAN_HELP = "help/territory help human";
 static const char * const TER_ELF_HELP = "help/territory help elf";

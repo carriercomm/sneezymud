5114d9eb649a656d6a3a69031987d7357687ee32
dash <dash@3801a427-e3ed-0310-b5ce-e9d0fb25575d>
2001-04-15 03:31:14 +0000

    guards don't call without shouting

--- code/spec_mobs.cc
+++ code/spec_mobs.cc
@@ -3074,7 +3074,8 @@ int cityguard(TBeing *, cmdTypeT cmd, const char *, TMonster *ch, TObj *)
   if (ch->fight() && ch->fight()->isPc() && ch->canSpeak()) {
     if (::number(0,99) < 65)
       return FALSE;   // have them shout a bit less 
-    if (ch->canSee(ch->fight())) {
+    bool targVis = ch->canSee(ch->fight());
+    if (TRUE) {
       // send the shout only if we can see person, but call guards regardless
       switch (number(1, 145)) {
         case 1:
@@ -3085,8 +3086,10 @@ int cityguard(TBeing *, cmdTypeT cmd, const char *, TMonster *ch, TObj *)
           strcpy(buf, "To me my fellows!  I need thy aid.");
           break;
         case 6:
-          sprintf(buf, "One of these days, %s, POW!  ZOOM!  To the moon!", ch->pers(ch->fight()));
-          break;
+	  if (targVis) {
+	    sprintf(buf, "One of these days, %s, POW!  ZOOM!  To the moon!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 7:
         case 8:
         case 9:
@@ -3111,23 +3114,31 @@ int cityguard(TBeing *, cmdTypeT cmd, const char *, TMonster *ch, TObj *)
           strcpy(buf, "Kawa Bunga!!");
           break;
         case 20:
-          sprintf(buf, "All these HORTS like %s will die to me and my COHORTS!!", ch->pers(ch->fight()));
-	  break;
+	  if (targVis) {
+	    sprintf(buf, "All these HORTS like %s will die to me and my COHORTS!!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 21:
         case 22:
         case 23:
-          sprintf(buf, "I need help! %s is attacking me at %s!", ch->pers(ch->fight()), ch->roomp->name);
-          break;
+	  if (targVis) {
+	    sprintf(buf, "I need help! %s is attacking me at %s!", ch->pers(ch->fight()), ch->roomp->name);
+	    break;
+	  }
         case 24:
         case 25:
         case 26:
-          sprintf(buf, "%s, I'm gonna rip off your head and puke down your neck!", ch->pers(ch->fight()));
-          break;
+	  if (targVis) {
+	    sprintf(buf, "%s, I'm gonna rip off your head and puke down your neck!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 27:
         case 28:
         case 29:
-          sprintf(buf, "%s must think %s is pretty tough to tangle with me!!", ch->pers(ch->fight()), ch->fight()->hssh());
-          break;
+          if (targVis) {
+	    sprintf(buf, "%s must think %s is pretty tough to tangle with me!!", ch->pers(ch->fight()), ch->fight()->hssh());
+	    break;
+	  }
         case 30:
         case 31:
         case 32:
@@ -3148,8 +3159,10 @@ int cityguard(TBeing *, cmdTypeT cmd, const char *, TMonster *ch, TObj *)
         case 43:
         case 44:
         case 45:
-          sprintf(buf, "Time to die, %s.", ch->pers(ch->fight()));
-          break;
+          if (targVis) {
+	    sprintf(buf, "Time to die, %s.", ch->pers(ch->fight()));
+	    break;
+	  }
         case 46:
         case 47:
         case 48:
@@ -3157,12 +3170,16 @@ int cityguard(TBeing *, cmdTypeT cmd, const char *, TMonster *ch, TObj *)
         case 50:
         case 51:
         case 52:
-          sprintf(buf, "You're dead meat, %s!", ch->pers(ch->fight()));
-          break;
+          if (targVis) {
+	    sprintf(buf, "You're dead meat, %s!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 53:
         case 54:
-          sprintf(buf, "The corpse of %s is about to be made available for looting at %s!", ch->pers(ch->fight()), ch->roomp->name);
-          break;
+          if (targVis) {
+	    sprintf(buf, "The corpse of %s is about to be made available for looting at %s!", ch->pers(ch->fight()), ch->roomp->name);
+	    break;
+	  }
         case 55:
           strcpy(buf, "Ack!  I am hit.  Avenge me, brethren!");
           break;
@@ -3171,8 +3188,10 @@ int cityguard(TBeing *, cmdTypeT cmd, const char *, TMonster *ch, TObj *)
         case 58:
         case 59:
         case 60:
-          sprintf(buf, "Clearly, %s wants to die!", ch->pers(ch->fight()));
-          break;
+          if (targVis) {
+	    sprintf(buf, "Clearly, %s wants to die!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 61:
           sprintf(buf, "Hey I'm kicking %s's ass right now...anyone want to join me?!", ch->pers(ch->fight()));
 	  break;
@@ -3185,41 +3204,53 @@ int cityguard(TBeing *, cmdTypeT cmd, const char *, TMonster *ch, TObj *)
           break;
         case 65:
         case 66:
-          sprintf(buf, "Foolish %s thinks %s can beat me!", ch->pers(ch->fight()), ch->fight()->hssh());
-          break;
+	  if (targVis) {
+	    sprintf(buf, "Foolish %s thinks %s can beat me!", ch->pers(ch->fight()), ch->fight()->hssh());
+	    break;
+	  }
         case 67:
         case 68:
         case 69:
-          sprintf(buf, "You think you're tough, %s?  Take that!", ch->pers(ch->fight()));
-          break;
+          if (targVis) {
+	    sprintf(buf, "You think you're tough, %s?  Take that!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 70:
         case 71:
         case 72:
           sprintf(buf, "Help!  Criminals at %s!", ch->roomp->name);
           break;
         case 73:
-          sprintf(buf, "Hey!! Come and check this out! %s is wearing pink chainmail! HAHAHAHAHA!!!", ch->pers(ch->fight()));
-	  break;
+	  if (targVis) {
+	    sprintf(buf, "Hey!! Come and check this out! %s is wearing pink chainmail! HAHAHAHAHA!!!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 74:
         case 75:
         case 76:
         case 77:
+          if (targVis) {
           sprintf(buf, "%s is going to have a VERY bad day once I finish with %s!", ch->pers(ch->fight()), ch->fight()->hmhr());
           break;
+	  }
         case 78:
         case 79:
         case 80:
           strcpy(buf, "I'm about to ROCK YOUR WORLD!");
           break;
         case 81:
-          sprintf(buf, "%s's face is about to be stamped into 200 gorrilla cookies!", ch->pers(ch->fight()));
-	  break;
+          if (targVis) {
+	    sprintf(buf, "%s's face is about to be stamped into 200 gorrilla cookies!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 82:
         case 83:
         case 84:
         case 85:
-          sprintf(buf, "They'll be picking up pieces of %s for weeks when I get finished with %s!", ch->pers(ch->fight()), ch->fight()->hmhr());
-          break;
+	  if (targVis) {
+	    sprintf(buf, "They'll be picking up pieces of %s for weeks when I get finished with %s!", ch->pers(ch->fight()), ch->fight()->hmhr());
+	    break;
+	  }
         case 86:
         case 87:
         case 88:
@@ -3228,8 +3259,10 @@ int cityguard(TBeing *, cmdTypeT cmd, const char *, TMonster *ch, TObj *)
         case 89:
         case 90:
         case 91:
-          sprintf(buf, "I'm your worst nightmare, %s!", ch->pers(ch->fight()));
-          break;
+	  if (targVis) {
+	    sprintf(buf, "I'm your worst nightmare, %s!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 92:
           strcpy(buf, "Chiefs!  Gimme some help... This mug be gacking me most heinously!");
           break;
@@ -3241,8 +3274,10 @@ int cityguard(TBeing *, cmdTypeT cmd, const char *, TMonster *ch, TObj *)
         case 96:
         case 97:
         case 98:
-          sprintf(buf, "I hate it when newbies, like %s, attack me!", ch->pers(ch->fight()));
-          break;
+          if (targVis) {
+	    sprintf(buf, "I hate it when newbies, like %s, attack me!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 99:
         case 100:
           strcpy(buf, "Eat some of this!");
@@ -3250,12 +3285,16 @@ int cityguard(TBeing *, cmdTypeT cmd, const char *, TMonster *ch, TObj *)
         case 101:
         case 102:
         case 103:
-          sprintf(buf, "It's time to remind %s just what a wimp %s is!", ch->pers(ch->fight()), ch->fight()->hssh());
-          break;
+          if (targVis) {
+	    sprintf(buf, "It's time to remind %s just what a wimp %s is!", ch->pers(ch->fight()), ch->fight()->hssh());
+	    break;
+	  }
         case 104:
         case 105:
-          sprintf(buf, "It's time to remind %s just what the words 'You wish your wounds would stop BLEEDING so much mean!!!", ch->pers(ch->fight()));
-          break;
+          if (targVis) {
+	    sprintf(buf, "It's time to remind %s just what the words 'You wish your wounds would stop BLEEDING so much mean!!!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 106:
         case 107:
         case 108:
@@ -3265,64 +3304,86 @@ int cityguard(TBeing *, cmdTypeT cmd, const char *, TMonster *ch, TObj *)
         case 110:
         case 111:
         case 112:
-          sprintf(buf, "%s is gonna die at my hands!", ch->pers(ch->fight()));
-          break;
+	  if (targVis) {
+	    sprintf(buf, "%s is gonna die at my hands!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 113:
         case 114:
         case 115:
-          sprintf(buf, "Just wait until my friends get here, %s!", ch->pers(ch->fight()));
-          break;
+          if (targVis) {
+	    sprintf(buf, "Just wait until my friends get here, %s!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 116:
-          sprintf(buf, "%s fights like a wombat!", ch->pers(ch->fight()));
-          break;
+          if (targVis) {
+	    sprintf(buf, "%s fights like a wombat!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 117:
-          sprintf(buf, "%s's momma wears combat boots!", ch->pers(ch->fight()));
-          break;
+          if (targVis) {
+	    sprintf(buf, "%s's momma wears combat boots!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 118:
         case 119:
         case 120:
-          sprintf(buf, "When I get through with %s, %s'll wish %s'd never heard the name %s!", ch->pers(ch->fight()), ch->fight()->hssh(), ch->fight()->hssh(), MUD_NAME);
-          break;
+          if (targVis) {
+	    sprintf(buf, "When I get through with %s, %s'll wish %s'd never heard the name %s!", ch->pers(ch->fight()), ch->fight()->hssh(), ch->fight()->hssh(), MUD_NAME);
+	    break;
+	  }
         case 121:
         case 122:
         case 123:
         case 124:
         case 125:
-          sprintf(buf, "Anybody want a piece of %s?  I'm tanking!", ch->pers(ch->fight()));
-          break;
+          if (targVis) {
+	    sprintf(buf, "Anybody want a piece of %s?  I'm tanking!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 126:
-        case 127:
-          sprintf(buf, "I hope you brought your recall scrolls with you, %s!  Cuz' you ain't walking away from this one!", ch->pers(ch->fight()));
-          break;
+	case 127:
+          if (targVis) {
+	    sprintf(buf, "I hope you brought your recall scrolls with you, %s!  Cuz' you ain't walking away from this one!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 128:
         case 129:
         case 130:
-          sprintf(buf, "Bandits and marauders at %s!  Help me destroy them!", ch->roomp->name);
-          break;
+          if (targVis) {
+	    sprintf(buf, "Bandits and marauders at %s!  Help me destroy them!", ch->roomp->name);
+	    break;
+	  }
         case 131:
-          sprintf(buf, "I'm gonna stomp %s's butt right out of %s!", ch->pers(ch->fight()), MUD_NAME);
-	  break;
+          if (targVis) {
+	    sprintf(buf, "I'm gonna stomp %s's butt right out of %s!", ch->pers(ch->fight()), MUD_NAME);
+	    break;
+	  }
         case 132:
         case 133:
         case 134:
         case 135:
-          sprintf(buf, "%s is a bloody coward!", ch->pers(ch->fight()));
-          break;
+          if (targVis) {
+	    sprintf(buf, "%s is a bloody coward!", ch->pers(ch->fight()));
+	    break;
+	  }
         case 136:
         case 137:
         case 138:
         case 139:
         case 140:
-          strcpy(buf, "'Tis but a flesh wound.");
-          break;
-        case 141:
+          if (targVis) {
+            sprintf(buf, "%s is going down!  HARD!", ch->pers(ch->fight()));
+            break;
+          }    
+	case 141:
         case 142:
         case 143:
         case 144:
-        case 145:
-          sprintf(buf, "%s is going down!  HARD!", ch->pers(ch->fight()));
-          break;
-        default:
+	case 145:
+	  strcpy(buf, "'Tis but a flesh wound.");
+	  break;
+	default:
           strcpy(buf, "Buggy ass code.  I puke on the coders!");
       }                                // end switch 
       if (!number(0, 20))
@@ -3334,7 +3395,7 @@ int cityguard(TBeing *, cmdTypeT cmd, const char *, TMonster *ch, TObj *)
         act(buf3, TRUE, ch, 0, 0, TO_ROOM);
       }
     } // can see check
-
+   
     if (ch->fight()) {
       for (t1 = ch->roomp->stuff; t1; t1 = t2) {
         t2 = t1->nextThing;
